e7a188e07c71ddb728a848839fb1b1ec56f30aaf
JD <jd@jdfant.com>
2020-05-27 15:22:23 -0600

    update

--- Rancher/rancher_clean.sh
+++ Rancher/rancher_clean.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 #
-# Cleanup Rancher and all remnants
+# Cleanup RKE/Rancher and all remnants
 #
 if [[ $EUID -ne 0 ]] ; then
     clear;echo -e "\nYou probably want to run this as root?\n"
     echo -e "Become root and try this again.\n"
     exit 1
 fi
-    
-docker stop $(docker ps -qa)
+
+docker stop $(docker container ls -q)
 docker rm -f $(docker ps -qa)
 docker volume rm $(docker volume ls -qf dangling=true)
 docker volume rm $(docker volume ls -q)
-docker network prune
-docker system prune
+#docker network prune
+#docker system prune
 
-docker rmi $(docker images -q)
+# Be kind to the docker repos ................
+# Will need to uncomment this when Rancher versions change.
+#docker rmi $(docker images -q)
 docker rm $(docker ps -q)
 
 rm -rf /etc/ceph \
